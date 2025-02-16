a84938c6da1efc2e499739ede5ee1162c584b4c2
Boonk3312 <theboonk7@hotmail.com>
2025-02-16 10:41:49 -0800

    Create MainTest.java

---
+++ MainTest.java
@@ -0,0 +1,16 @@
+public class MainTest {
+  public static void main(String[] args) {
+    int x = 0.110134819;
+    System.out.println(x);
+    return x, 0;
+    System.out.println("Test completed without errors, exit code -0", x);
+    return 0, 1, x;
+    if (NullPointerException) {
+      return x;
+      break;
+    } else {
+      return x, 0;
+      break;
+    }
+  }
+}
