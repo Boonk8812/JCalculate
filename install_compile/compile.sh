7717316063ab93835888db06ac805cfd87f81fd3
Boonk3312 <theboonk7@hotmail.com>
2025-02-16 12:04:07 -0800

    Create compile.sh

---
+++ install_compile/compile.sh
@@ -0,0 +1,5 @@
+javac MainTest.java
+javac Main.java
+echo You will find the files in this directory.
+echo If you want to run this, enter: java Main
+echo If you see errors, report them in the discussions.
